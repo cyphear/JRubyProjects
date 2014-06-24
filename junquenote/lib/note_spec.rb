fb340363eaafa97ffef39a967ebaeec26a3a4926
cyphear <cyphear@gmail.com>
2014-06-23 22:50:40 -0600

    Added note_spec.rb for Rspec testing

---
+++ junquenote/lib/note_spec.rb
@@ -0,0 +1,21 @@
+# To change this license header, choose License Headers in Project Properties.
+# To change this template file, choose Tools | Templates
+# and open the template in the editor.
+
+#require 'note'
+#
+#describe Note do
+#  before(:each) do
+#    @note = Note.new
+#  end
+#
+#  it "should desc" do
+#    # TODO
+#  end
+#end
+
+describe 'The main window' do
+  it 'launches with a welcome message'
+  it 'exits without a prompt if nothing has changed'
+  it 'prompts before exiting if the document has changed'
+end
